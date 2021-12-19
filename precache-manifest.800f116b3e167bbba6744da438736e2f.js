aaadfc71f8bf478891a62b9282e35a5ea23d5354
jsswp2004 <31414054+jsswp2004@users.noreply.github.com>
2021-12-19 01:04:47 -0500

    Updates

---
+++ precache-manifest.800f116b3e167bbba6744da438736e2f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71369cf7bc284b8962e09b4cd7e28b9a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "897b0d6d6c1e1d08132e",
+    "url": "/monsters-rolodex/static/css/main.2744bffb.chunk.css"
+  },
+  {
+    "revision": "c8e56e1d6cf27670d4ed",
+    "url": "/monsters-rolodex/static/js/2.580db2ef.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/monsters-rolodex/static/js/2.580db2ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d48a9730b00b42723b60",
+    "url": "/monsters-rolodex/static/js/3.1119309c.chunk.js"
+  },
+  {
+    "revision": "897b0d6d6c1e1d08132e",
+    "url": "/monsters-rolodex/static/js/main.7ff08bda.chunk.js"
+  },
+  {
+    "revision": "305fc5360722635b1f2c",
+    "url": "/monsters-rolodex/static/js/runtime-main.b3b2c6c5.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/monsters-rolodex/static/media/logo.06e73328.svg"
+  }
+]);
